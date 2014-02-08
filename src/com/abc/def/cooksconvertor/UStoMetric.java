720cf22bff43acf875319462d83eeba5a7eedfcf
Zoe Nolan <github@zoenolan.org>
2014-02-08 20:57:10 +0000

    Added the a second activtiy

--- src/com/abc/def/cooksconvertor/UStoMetric.java
+++ src/com/abc/def/cooksconvertor/UStoMetric.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class UStoMetric extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_usto_metric);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.usto_metric, menu);
 		return true;
 	}
 
