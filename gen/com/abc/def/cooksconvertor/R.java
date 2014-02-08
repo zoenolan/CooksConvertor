4a7ca3e8b473db5eb3ef0c5f3fe4906bbb7f40b6
Zoe Nolan <github@zoenolan.org>
2014-02-08 21:04:19 +0000

    Only allowed a single java file

--- gen/com/abc/def/cooksconvertor/R.java
+++ gen/com/abc/def/cooksconvertor/R.java
@@ -28,7 +28,6 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_usto_metric=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
